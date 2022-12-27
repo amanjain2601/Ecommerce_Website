34bef8608957b4861e178656c26c9ac48e4294fa
amanjain2601 <amanjain2601@gmail.com>
2022-12-27 19:20:38 +0530

    Designing home page and product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from './component/layout/Header/Header.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WebFont from 'webfontloader';
 import React from 'react';
 import Footer from './component/layout/Footer/Footer.js';
+import Home from './component/Home/Home.js';
 
 function App() {
   React.useEffect(() => {
@@ -15,10 +16,13 @@ function App() {
   });
 
   return (
-    <Router>
+    <div>
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </Router>
+    </div>
   );
 }
 

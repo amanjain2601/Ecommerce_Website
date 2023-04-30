fa50ad60fe88e2a7d6eb336e9caada65648a9732
amanjain2601 <amanjain2601@gmail.com>
2023-04-30 19:23:13 +0530

    Modifiy few backend code for deploy purpose

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is working on http://localhost:${port}`);
 });
 
 // Unhandled promise rejection

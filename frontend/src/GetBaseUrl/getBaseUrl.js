874a079e1d75964d65878b3219e1385e3ccb0532
amanjain2601 <amanjain2601@gmail.com>
2023-07-03 00:21:34 +0530

    Did some minor code restructuring

--- frontend/src/GetBaseUrl/getBaseUrl.js
+++ frontend/src/GetBaseUrl/getBaseUrl.js
@@ -1,5 +1,5 @@
 // for deploying
-export const BASE_URL = 'https://ecommerceapp-backend-3f2f.onrender.com';
+export const BASE_URL = 'https://ecommerceapp-backend-d80p.onrender.com';
 
 // for running on your local machine
 // export const BASE_URL = 'http://localhost:4000';
